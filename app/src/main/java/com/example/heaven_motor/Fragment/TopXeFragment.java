f8c28ee1b234a6484ea0a176d3da6b24b174c4ca
This MC <sinhdtph27866@fpt.edu,vn>
2022-11-14 15:27:07 +0700

    sinhdtph27866_themFragment

---
+++ app/src/main/java/com/example/heaven_motor/Fragment/TopXeFragment.java
@@ -0,0 +1,43 @@
+package com.example.heaven_motor.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.heaven_motor.R;
+
+
+public class TopXeFragment extends Fragment {
+
+
+    public TopXeFragment() {
+        // Required empty public constructor
+    }
+
+    public static TopXeFragment newInstance() {
+        TopXeFragment fragment = new TopXeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top_xe, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
