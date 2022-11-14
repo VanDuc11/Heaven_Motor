728d73bdde529f669aab3797d2a1b2b231c11248
This MC <sinhdtph27866@fpt.edu,vn>
2022-11-14 21:25:52 +0700

    sinhdtph27866_themFragment

--- app/src/main/java/com/example/heaven_motor/Fragment/QuanLiNguoiDungFragment.java
+++ app/src/main/java/com/example/heaven_motor/Fragment/QuanLiNguoiDungFragment.java
@@ -1,19 +1,20 @@
 package com.example.heaven_motor.Fragment;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.example.heaven_motor.R;
 
 
-
 public class QuanLiNguoiDungFragment extends Fragment {
+
 
 
     public QuanLiNguoiDungFragment() {
